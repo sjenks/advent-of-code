a0c8af541fd630a768cd354540f61114a7d72431
Scott Jenks <sjenks@gmail.com>
2019-12-07 12:36:45 -0800

    Day 7, part 2

--- 2019/day07/driver.py
+++ 2019/day07/driver.py
@@ -2,6 +2,33 @@ import itertools
 from IntCodeVm import IntCodeVm
 
 def main():
+	ampInput = [5, 6, 7, 8 ,9]
+	maxOutput = -9999999
+	for it in itertools.permutations(ampInput):
+		ampIn = 0
+		vms = []
+		#initialize vms
+		for phase in it:
+			vm = IntCodeVm("intcode.txt")
+			vm.input = phase
+			vm.resume()
+			vms.append(vm)
+
+		allHalted = False
+		while (not allHalted):
+			allHalted = True
+			for vm in vms:
+				if (not vm.halted):
+					vm.input = ampIn
+					vm.resume()
+					ampIn = vm.output
+					allHalted = False
+
+		if ampIn > maxOutput:
+			maxOutput = ampIn
+	print("max output found ", maxOutput)
+
+def partOne():
 	ampInput = [0, 1, 2, 3, 4]
 	maxOutput = -9999999
 	for it in itertools.permutations(ampInput):
