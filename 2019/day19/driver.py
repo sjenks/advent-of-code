f938eda8e70b6795739ff02e897228007c2eb08d
Scott Jenks <sjenks@gmail.com>
2019-12-21 19:21:26 -0800

    Added day 19 part 2

--- 2019/day19/driver.py
+++ 2019/day19/driver.py
@@ -1,6 +1,7 @@
 from IntCodeVm import IntCodeVm
 from enum import Enum
 import time
+import queue
 
 class Driver:
 	def readFile(self):
@@ -11,26 +12,61 @@ class Driver:
 		self.code = inputStr
 
 	def main(self):
-		self.grid = []
-		self.readFile()
-		for y in range(50):
-			row = []
-			for x in range(50):
-				row.append(0)
-			self.grid.append(row)
-		
-		positionGenerator = self.positionGen
+		self.grid = {}
+		self.readFile()		
+		bottomGenerator = self.probeBottom()
 
-		for position in positionGenerator():
+		while True:
+			position = next(bottomGenerator)
 			self.readPosition(position[0], position[1])
-		self.printGrid()
 
-	def positionGen(self):
-		for y in range(len(self.grid)):
-			for x in range(len(self.grid[0])):	
-				yield((x,y))
+	def probeBottom(self):
+		next = (11, 10)
+
+		#go down one, and right until back in the #'s
+		while(True):
+			next = (next[0], next[1] + 1)
+			if(next not in self.grid):
+				yield next
+			
+			while next not in self.grid or self.grid[next] == 0:
+				if next not in self.grid:
+					yield next
+				elif self.grid[next] == 0:
+					next = (next[0] + 1, next[1])
+			
+			##### check the spot
+			for i in range(150):
+				pos = (next[0] + i, next[1])
+				squareGen = self.checkIf100Sq(pos)
+				for i in squareGen:
+					yield i
+
+
+	def checkIf100Sq(self, position):
+		current = position
+		lineSize = 100
+		found = False
+		while not found:
+			#fill in lines
+			for x in range(lineSize):
+				next = (current[0] + x, current[1])
+				if next not in self.grid:
+					yield next
+				if self.grid[next] == 0:
+					return False
+			for y in range(lineSize):
+				next = (current[0], current[1] + y)
+				if next not in self.grid:
+					yield next
+				if self.grid[next] == 0:
+					return False
+			
+			print("found it? ", current)
+			exit(1)
 
 	def readPosition(self, x, y):
+		print('reading', x, y)
 		vm = IntCodeVm(None, self.code)
 		readX = True
 		for i in vm.run():
@@ -41,8 +77,8 @@ class Driver:
 					vm.input = y
 				readX = not readX
 			else:
-				self.grid[y][x] = vm.output
-		self.grid[y][x] = vm.output
+				self.grid[(x,y)] = vm.output
+		self.grid[(x,y)] = vm.output
 
 	def printGrid(self):
 		paintCnt = 0
